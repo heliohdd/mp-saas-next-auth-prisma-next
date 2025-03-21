40638e1dc97d8dda6888548780bdd055337d3e5b
Helio Dourado <heliohdd@yahoo.com.br>
2025-03-21 17:01:13 -0300

    RegisterAction to Create New User

--- app/(auth)/cadastro/page.tsx
+++ app/(auth)/cadastro/page.tsx
@@ -7,7 +7,9 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Form from "next/form";
 import Link from "next/link";
+import registerAction from "@/lib/registerAction";
 
 export default async function RegisterPage() {
   return (
@@ -18,23 +20,25 @@ export default async function RegisterPage() {
           <CardDescription>Faça seu cadastro gratuitamente.</CardDescription>
         </CardHeader>
         <CardContent>
-          <div>
-            <Label>Nome</Label>
-            <Input type="text" name="name" placeholder="Fulano de Tal" />
-          </div>
-          <div>
-            <Label>Email</Label>
-            <Input type="email" name="email" placeholder="eu@exemplo.com" />
-          </div>
-          <div>
-            <Label>Senha</Label>
-            <Input type="password" name="password" placeholder="********" />
-          </div>
-          <div>
-            <Button className="w-full mt-6" type="submit">
-              Registrar
-            </Button>
-          </div>
+          <Form action={registerAction}>
+            <div>
+              <Label>Nome</Label>
+              <Input type="text" name="name" placeholder="Fulano de Tal" />
+            </div>
+            <div>
+              <Label>Email</Label>
+              <Input type="email" name="email" placeholder="eu@exemplo.com" />
+            </div>
+            <div>
+              <Label>Senha</Label>
+              <Input type="password" name="password" placeholder="********" />
+            </div>
+            <div>
+              <Button className="w-full mt-6" type="submit">
+                Registrar
+              </Button>
+            </div>
+          </Form>
         </CardContent>
       </Card>
       <p className="text-sm text-muted-foreground mt-3">
---
+++ lib/registerAction.ts
@@ -0,0 +1,18 @@
+"use server";
+
+export default async function registerAction(formData: FormData) {
+  const entries = Array.from(formData.entries());
+  const data = Object.fromEntries(entries);
+
+  console.log("==== Server Action Register User ====");
+  console.log(data);
+
+  // Se não tiver "Nome", "Email" ou "Senha", retornar erro
+  if (!data.name || !data.email || !data.password) {
+    throw new Error("Você precisa passar todos os dados!!!");
+  }
+
+  // Se um usuário já existir, retornar erro
+
+  // Se usuário não existir no banco de dados criar novo usuário
+}
