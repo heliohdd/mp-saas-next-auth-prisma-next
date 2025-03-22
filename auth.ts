2c61bcaed1e65eb0004c8ffc96e4c305e4c3bf44
Helio Dourado <heliohdd@yahoo.com.br>
2025-03-22 14:47:32 -0300

    Email and Password Login Configuration at NextAuth

--- auth.ts
+++ auth.ts
@@ -1,5 +1,22 @@
+import Credentials from "next-auth/providers/credentials";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: { email: {}, password: {} },
+      authorize: async (credentials) => {
+        console.log(credentials);
+        // Lógica de autenticação
+        // Se não autenticado retorna 'null'
+        // Se autenticado retorna 'user'
+
+        return {
+          name: "Helio",
+          email: "helio@teste.com",
+          password: "asdçfklj",
+        };
+      },
+    }),
+  ],
 });
